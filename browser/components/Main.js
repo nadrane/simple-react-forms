02fdcc09f16073d2e26138d21e3714e46bd74413
Nick Drane <nicholasdrane@gmail.com>
2017-02-07 14:31:46 -0600

    start

--- browser/components/Main.js
+++ browser/components/Main.js
@@ -3,83 +3,27 @@ import React from 'react';
 class UserForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-        username: {value: '', dirty: false},
-        password: {value: '', dirty: false},
-        confirm: {value: '', dirty: false}
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.isFormValid = this.isFormValid.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    isFormValid() {
-      return this.isUsernameValid() && this.arePasswordsSame();
-    }
-
-    handleChange(e) {
-      const field = e.target.name;
-      const newField = {value: e.target.value, dirty: true};
-      this.setState({[field]: newField});
-    }
-
-    handleSubmit(e) {
-      e.preventDefault();
-      if (this.isFormValid()) {
-        this.props.updateUsers(this.state.username.value);
-        this.setState({
-          username: {value: '', dirty: false},
-          password: {value: '', dirty: false},
-          confirm: {value: '', dirty: false}
-        });
-      }
-    }
-
-    isUsernameValid() {
-      return this.state.username.value.length !== 0;
-    }
-
-    showUsernameError() {
-      if (this.state.username.dirty) {
-        return !this.isUsernameValid();
-      } else {
-        return false;
-      }
-    }
-
-    arePasswordsSame() {
-      return (this.state.password.value === this.state.confirm.value);
-    }
-
-    showPasswordError() {
-      if (this.state.password.dirty && this.state.confirm.dirty) {
-        return !this.arePasswordsSame();
-      } else {
-        return false;
-      }
     }
 
     render() {
       return (
         <div className="container">
-          <div className="error">
-            {this.showUsernameError() ? <p>Username must not be blank</p> : null}
-            {this.showPasswordError() ? <p>Passwords must be the same</p> : null}
+          <div className="errors">
           </div>
-          <form onSubmit={this.handleSubmit}>
+          <form>
             <div className="form-group">
               <label>Enter username</label>
-              <input onChange={this.handleChange} className="form-control" name="username" value={this.state.username.value}/>
+              <input className="form-control" name="username" />
             </div>
             <div className="form-group">
               <label>Enter password</label>
-              <input onChange={this.handleChange} className="form-control" name="password" value={this.state.password.value}/>
+              <input className="form-control" name="password" />
             </div>
             <div className="form-group">
               <label>Confirm password</label>
-              <input onChange={this.handleChange} className="form-control" name="confirm" value={this.state.confirm.value}/>
+              <input className="form-control" name="confirm" />
             </div>
-            <button disabled={!this.isFormValid()} className="btn btn-primary" type="submit">Make User</button>
+            <button className="btn btn-primary" type="submit">Make User</button>
           </form>
           <div className="row">
             { this.props.users.map((user, i) => <div key={i} className="col-xs-12">{user}</div> )}
@@ -92,18 +36,12 @@ class UserForm extends React.Component {
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {users: []};
-    this.updateUsers = this.updateUsers.bind(this);
-  }
-
-  updateUsers(username) {
-    const currentUsers = this.state.users;
-    this.setState({users: [...currentUsers, username]});
+    this.state = {users: []}
   }
 
   render() {
     return (
-      <UserForm updateUsers={this.updateUsers} users={this.state.users} />
+      <UserForm users={this.state.users} />
     );
   }
 }
